b88d40a39eae0e80e68cceb575755a8ec673ca61
Gianluca <gianluca.bombelli.work@gmail.com>
2022-10-29 21:19:07 +0200

    menu-almost-finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LinkComponent } from './link/link.component';
+import { HomeComponent } from './home/home.component';
+import { ContattiComponent } from './contatti/contatti.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
+import { EventoComponent } from './evento/evento.component';
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'contatti', component: ContattiComponent},
+  {path:'chi-siamo', component: ChiSiamoComponent},
+  {path:'evento', component:EventoComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinkComponent
+    LinkComponent,
+    HomeComponent,
+    ContattiComponent,
+    ChiSiamoComponent,
+    EventoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule,
+    RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule { }
+export class AppRoutingModule { }
